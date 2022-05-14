b41b6b10394873daa3aa9931e88e2ed83faf418d
JR N <49070206+xiaoxiaozhihuishu@users.noreply.github.com>
2022-05-14 09:42:16 +0800

    Create new_chrome.m

---
+++ MFMO/NMI-NSGAII/new_chrome.m
@@ -0,0 +1,42 @@
+function f  = new_chrome(intermediate_chromosome,pop)
+
+N = size(intermediate_chromosome,1);
+
+% V is the number of features, which is also the chromosome dimension
+% M is the dimension of the objective function
+global V;
+M = 2;
+
+[temp,index] = sort(intermediate_chromosome(:,M + V + 1));
+
+for i = 1 : N
+    sorted_chromosome(i,:) = intermediate_chromosome(index(i),:);
+end
+
+max_rank = max(intermediate_chromosome(:,M + V + 1));
+previous_index = 0;
+
+for i = 1 : max_rank
+    current_index = max(find(sorted_chromosome(:,M + V + 1) == i));
+    if current_index > pop
+        remaining = pop - previous_index;
+        temp_pop = ...
+            sorted_chromosome(previous_index + 1 : current_index, :);
+        [temp_sort,temp_sort_index] = ...
+            sort(temp_pop(:, M + V + 2),'descend');
+        
+        for j = 1 : remaining
+            f(previous_index + j,:) = temp_pop(temp_sort_index(j),:);
+        end
+        return;
+    elseif current_index < pop
+        f(previous_index + 1 : current_index, :) = ...
+            sorted_chromosome(previous_index + 1 : current_index, :);
+    else
+        f(previous_index + 1 : current_index, :) = ...
+            sorted_chromosome(previous_index + 1 : current_index, :);
+        return;
+    end
+    previous_index = current_index;
+end
+end
